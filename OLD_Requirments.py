aece77447c7064780dbc08ce9145821f450746f2
asolovykh <solovykh.aa19@physics.msu.ru>
2025-11-06 13:35:09 +0300

    Old files removed.

--- OLD_Requirments.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-
-print(subprocess.check_output(f'py -m ensurepip --upgrade', shell=True, universal_newlines=True))
-dependencies = 'freetype-py, matplotlib, matplotlib-inline, numpy, pandas, paramiko, Pillow, psgtray, pygame, PyOpenGL, PyOpenGL-accelerate, pypiwin32, PySimpleGUI, PySimpleGUIQt, pywin32, pywin32-ctypes, simplejson, XlsxWriter'
-dependencies = dependencies.split(', ')
-for dependence in dependencies:
-    print(subprocess.check_output(f'pip install {dependence}', shell=True, universal_newlines=True))
